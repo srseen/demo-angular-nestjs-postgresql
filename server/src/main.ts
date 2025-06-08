13ff3d70cc6d732a65482e95478b10b81c9bea33
srseen <seereen.2540@gmail.com>
2025-06-08 12:04:08 +0700

    fix(main): improve error handling during application bootstrap

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(); // เปิด CORS
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+bootstrap().catch((err) => {
+  console.error('Error during bootstrap:', err);
+});
